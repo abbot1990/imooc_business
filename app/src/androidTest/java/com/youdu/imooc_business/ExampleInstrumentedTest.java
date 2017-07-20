38b67e56550c5569001c64fafb415e636263b391
fangzheng <185849480@qq.com>
2017-07-21 00:46:15 +0800

    no message

--- app/src/androidTest/java/com/youdu/imooc_business/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/youdu/imooc_business/ExampleInstrumentedTest.java
@@ -4,8 +4,17 @@ import android.content.Context;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 
+import com.youdu.imooc_business.okhttp.CommonOkHttpClient;
+import com.youdu.imooc_business.okhttp.request.CommonRequest;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
 
 import static org.junit.Assert.*;
 
@@ -23,4 +32,7 @@ public class ExampleInstrumentedTest {
 
         assertEquals("com.youdu.imooc_business", appContext.getPackageName());
     }
+
+
+
 }
--- app/src/test/java/com/youdu/imooc_business/ExampleUnitTest.java
+++ app/src/test/java/com/youdu/imooc_business/ExampleUnitTest.java
@@ -1,6 +1,15 @@
 package com.youdu.imooc_business;
 
+import com.youdu.imooc_business.okhttp.CommonOkHttpClient;
+import com.youdu.imooc_business.okhttp.request.CommonRequest;
+
 import org.junit.Test;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
 
 import static org.junit.Assert.*;
 
@@ -14,4 +23,23 @@ public class ExampleUnitTest {
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
     }
+
+    @Test
+    public void testOkHttp(){
+        String url = "http://www.baidu.com";
+        System.out.println("true");
+        CommonOkHttpClient.sendRequest(CommonRequest.createGetRequest(url, null), new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+
+                System.out.println(response.body().string());
+            }
+        });
+    }
+
 }
