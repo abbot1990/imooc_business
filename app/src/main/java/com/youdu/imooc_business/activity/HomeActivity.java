b0098aa6507048f1de05853e9b5e8e3528068f11
fangzheng <185849480@qq.com>
2017-07-20 21:32:37 +0800

    no message

--- app/src/main/java/com/youdu/imooc_business/activity/HomeActivity.java
+++ app/src/main/java/com/youdu/imooc_business/activity/HomeActivity.java
@@ -1,11 +1,9 @@
 package com.youdu.imooc_business.activity;
 
+import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.DisplayMetrics;
-import android.util.Log;
 import android.view.View;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -13,8 +11,10 @@ import android.widget.TextView;
 import com.youdu.imooc_business.R;
 import com.youdu.imooc_business.activity.base.BaseActivity;
 import com.youdu.imooc_business.view.fragment.HomeFragment;
+import com.youdu.imooc_business.view.fragment.MessageFragment;
+import com.youdu.imooc_business.view.fragment.MineFragment;
 
-public class HomeActivity extends BaseActivity implements View.OnClickListener{
+public class HomeActivity extends BaseActivity implements View.OnClickListener {
 
     private FragmentManager mFragmentManager;
 
@@ -27,6 +27,10 @@ public class HomeActivity extends BaseActivity implements View.OnClickListener{
     private TextView mMessageImageView;
     private TextView mMineImageView;
 
+    private Fragment mHomeFragment;
+    private Fragment mMessageFragment;
+    private Fragment mMineFragment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,7 +41,8 @@ public class HomeActivity extends BaseActivity implements View.OnClickListener{
         //默认fragment
         mFragmentManager = getFragmentManager();
         FragmentTransaction fragmentTransaction = mFragmentManager.beginTransaction();
-        fragmentTransaction.replace(R.id.content_layout,new HomeFragment());
+        mHomeFragment = new HomeFragment();
+        fragmentTransaction.replace(R.id.content_layout, mHomeFragment);
         fragmentTransaction.commit();
         mHomeImageView.setBackgroundResource(R.drawable.comui_tab_home_selected);
 
@@ -45,20 +50,32 @@ public class HomeActivity extends BaseActivity implements View.OnClickListener{
     }
 
     //初始化view
-    private void initView(){
-        mHomeLayoutView = (RelativeLayout)findViewById(R.id.home_layout_view);
-        mPondLayoutView = (RelativeLayout)findViewById(R.id.pond_layout_view);
-        mMessageLayoutView = (RelativeLayout)findViewById(R.id.message_layout_view);
-        mMineLayoutView = (RelativeLayout)findViewById(R.id.mine_layout_view);
+    private void initView() {
+        mHomeLayoutView = (RelativeLayout) findViewById(R.id.home_layout_view);
+        mPondLayoutView = (RelativeLayout) findViewById(R.id.pond_layout_view);
+        mMessageLayoutView = (RelativeLayout) findViewById(R.id.message_layout_view);
+        mMineLayoutView = (RelativeLayout) findViewById(R.id.mine_layout_view);
+        mHomeLayoutView.setOnClickListener(this);
+        mPondLayoutView.setOnClickListener(this);
+        mMessageLayoutView.setOnClickListener(this);
+        mMineLayoutView.setOnClickListener(this);
 
-        mHomeImageView = (TextView)findViewById(R.id.home_image_view);
-        mMessageImageView = (TextView)findViewById(R.id.message_image_view);
-        mMineImageView = (TextView)findViewById(R.id.mine_image_view);
+        mHomeImageView = (TextView) findViewById(R.id.home_image_view);
+        mMessageImageView = (TextView) findViewById(R.id.message_image_view);
+        mMineImageView = (TextView) findViewById(R.id.mine_image_view);
+    }
+
+    private void hideFragment(Fragment fragment, FragmentTransaction fragmentTransaction) {
+        if (fragment == null) {
+            return;
+        }
+        fragmentTransaction.hide(fragment);
     }
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
+        FragmentTransaction fragmentTransaction = mFragmentManager.beginTransaction();
+        switch (v.getId()) {
             case R.id.home_layout_view:
 
                 mHomeImageView.setBackgroundResource(R.drawable.comui_tab_home_selected);
@@ -66,16 +83,55 @@ public class HomeActivity extends BaseActivity implements View.OnClickListener{
                 mMessageImageView.setBackgroundResource(R.drawable.comui_tab_message);
                 mMineImageView.setBackgroundResource(R.drawable.comui_tab_person);
 
+                hideFragment(mMessageFragment, fragmentTransaction);
+                hideFragment(mMineFragment, fragmentTransaction);
+                if (mHomeFragment == null) {
+                    mHomeFragment = new HomeFragment();
+                    fragmentTransaction.add(R.id.content_layout,mHomeFragment);
+                }else{
+                    fragmentTransaction.show(mHomeFragment);
+                }
+
+
                 break;
             case R.id.pond_layout_view:
+                mHomeImageView.setBackgroundResource(R.drawable.comui_tab_home);
+                mMessageImageView.setBackgroundResource(R.drawable.comui_tab_message_selected);
+                mMineImageView.setBackgroundResource(R.drawable.comui_tab_person);
+
 
                 break;
             case R.id.message_layout_view:
+                mHomeImageView.setBackgroundResource(R.drawable.comui_tab_home);
+                mMessageImageView.setBackgroundResource(R.drawable.comui_tab_message_selected);
+                mMineImageView.setBackgroundResource(R.drawable.comui_tab_person);
+
+                hideFragment(mHomeFragment, fragmentTransaction);
+                hideFragment(mMineFragment, fragmentTransaction);
+                if (mMessageFragment == null) {
+                    mMessageFragment = new MessageFragment();
+
+                    fragmentTransaction.add(R.id.content_layout,mMessageFragment);
+                }else{
+                    fragmentTransaction.show(mMessageFragment);
+                }
 
                 break;
             case R.id.mine_layout_view:
+                mHomeImageView.setBackgroundResource(R.drawable.comui_tab_home);
+                mMessageImageView.setBackgroundResource(R.drawable.comui_tab_message);
+                mMineImageView.setBackgroundResource(R.drawable.comui_tab_person_selected);
 
+                hideFragment(mHomeFragment, fragmentTransaction);
+                hideFragment(mMessageFragment, fragmentTransaction);
+                if (mMineFragment == null) {
+                    mMineFragment = new MineFragment();
+                    fragmentTransaction.add(R.id.content_layout,mMineFragment);
+                }else{
+                    fragmentTransaction.show(mMineFragment);
+                }
                 break;
         }
+        fragmentTransaction.commit();
     }
 }
